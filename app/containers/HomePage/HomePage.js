de493f93d1477c8d78ed6729b43d54c9ec66095e
Long Hoang <44691721+longhpzigvy@users.noreply.github.com>
2019-04-12 14:40:34 +0700

    Init mia real agent

---
+++ app/containers/HomePage/HomePage.js
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import HomePage from '../../components/HomePage';
+
+export default connect()(HomePage);
---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'styled-components',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
+  env: {
+    production: {
+      only: ['app'],
+      plugins: [
+        'lodash',
+        'transform-react-remove-prop-types',
+        '@babel/plugin-transform-react-inline-elements',
+        '@babel/plugin-transform-react-constant-elements',
+      ],
+    },
+  },
+};
