6446591462272153c2d08604d9496cc631a45c91
Long Hoang <44691721+longhpzigvy@users.noreply.github.com>
2019-04-19 14:35:40 +0700

    Complete simple landing page

--- app/components/HomePage/HomePage.js
+++ app/components/HomePage/HomePage.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {
+  MiaWrapper,
+  MiaTitle,
+  MiaIntro,
+  MiaIntroStatic,
+  MiaMessengerLink,
+  MiaMessengerImg,
+  MiaLinkText,
+  MiaLoginText,
+  MiaFBLogin,
+} from './styles';
 
-const HomePage = () => {
-  return (
-    <div>
-      Miaw! Miaw!
-    </div>
-  )
-}
+const HomePage = () => (
+  <MiaWrapper>
+    <MiaTitle>Mia Consults</MiaTitle>
+    <MiaIntro>
+      <MiaIntroStatic>More than a chat bot</MiaIntroStatic>
+    </MiaIntro>
+    <MiaMessengerLink
+      href="https://www.messenger.com/t/1620637394734647"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <MiaMessengerImg src="/assets/images/fb-messenger-icon.png" alt="Messenger Logo" />
+      <MiaLinkText>Chat with Mia</MiaLinkText>
+    </MiaMessengerLink>
+    <MiaFBLogin>
+      Or
+      <MiaLoginText href="api/auth/login/facebook">
+        Login with Facebook
+      </MiaLoginText>
+    </MiaFBLogin>
+  </MiaWrapper>
+);
+
 
 export default HomePage;
