3fef358292bfacf8595d4e7f475bc05fe77a1f72
Long Hoang <44691721+longhpzigvy@users.noreply.github.com>
2019-04-22 15:48:05 +0700

    Complete simple training box

---
+++ app/components/TrainingBox/styles.js
@@ -0,0 +1,142 @@
+import styled, { css } from 'styled-components';
+
+export const TrainWrapper = styled.div`
+  width: 980px;
+  height: 100%;
+  background: #fafafa;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+
+export const TrainTitle = styled.h3`
+  font-size: 24px;
+  margin-bottom: 5px;
+`;
+
+export const TrainSubTitle = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const TrainMiaResponseInput = styled.input`
+  background: rgb(238, 238, 238);
+  width: 100%;
+  height: 35px;
+  margin-bottom: 15px;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #ccc;
+  padding-left: 5px;
+  font-size: 18px;
+
+  ::placeholder {
+    color: #aaa;
+    font-style: normal;
+  }
+`;
+
+export const TrainerUserInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 35px;
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const TrainerUserInputPlaceHolder = styled.div`
+  position: absolute;
+  color: #aaa;
+  margin-left: 5px;
+  font-size: 18px;
+`;
+
+export const TrainUserInput = styled.div`
+  background: rgb(238, 238, 238);
+  width: 100%;
+  height: 35px;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #ccc;
+  padding-left: 5px;
+  padding-top: 7.5px;
+  font-size: 18px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const TrainValidateBtn = styled.button`
+  box-shadow: none;
+  outline: none;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 7.5px 10px;
+  font-size: 16px;
+  transition: .3s ease;
+  margin-top: 5px;
+  & > i:before {
+    margin-right: 5px;
+    font-weight: bold;
+  }
+
+  ${props => props.active ? css`
+    background: #90cb66;
+    color: #003a02;
+  ` : css`
+    background: #eee;
+    color: #ccc;
+  `}
+
+  &:hover {
+    ${props => props.active ? css`
+      background: #74be40;
+      box-shadow: 0 0 1px 2px #90cb66;
+  ` : css``}
+  }
+`;
+
+export const TrainEntityInput = styled.input`
+  border: none;
+  background: none;
+  height: 30px;
+  padding-left: 5px;
+  font-size: 16px;
+  ::placeholder {
+    color: #aaa;
+    font-style: normal;
+  }
+`;
+
+export const TrainAddEntityBtn = styled.button`
+  background: black;
+  border: none;
+  box-shadow: none;
+  outline: none;
+  width: 15px;
+  height: 15px;
+  border-radius: 100%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5px;
+  i:before {
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+  }
+`;
+
+export const TrainEntityWrapper = styled.div`
+  margin-bottom: 15px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
---
+++ app/pages/BotTraining/BotTraining.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TrainingBox from '../../components/TrainingBox';
+import { BotTrainningWrapper } from './styles';
+
+const BotTraining = () => (
+  <BotTrainningWrapper>
+    <TrainingBox />
+  </BotTrainningWrapper>
+);
+
+export default BotTraining;
