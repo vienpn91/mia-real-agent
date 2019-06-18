da90d5613fd81b5cd992b39a1dd47aa92dce5ead
swomfire <vucuongJim@gmail.com>
2019-06-18 15:37:02 +0700

    Remove bot (still there ?)

--- app/containers/EntityList/EntityList.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import EntityList from '../../components/EntityList';
-import { getUserEntity } from '../../reducers/entities';
-
-const mapStateToProps = state => ({
-  entityList: getUserEntity(state),
-});
-
-export default connect(mapStateToProps)(EntityList);
