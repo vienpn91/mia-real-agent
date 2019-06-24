a323a78d923ccf9ebfded61123f66e08b2bd53ce
Tri Nguyen <trinm@zigvy.com>
2019-06-24 17:05:36 +0700

    Change /home to /ticket

---
+++ app/containers/Dashboard/Dashboard.js
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import Dashboard from '../../components/Dashboard';
+
+export default connect()(Dashboard);
--- app/routers.js
+++ app/routers.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import UnauthRoute from './containers/Route/UnauthenticateRoute';
 import HomePage from './pages/HomePage';
+import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
 import LoginCallBackPage from './pages/LoginCallback';
@@ -18,7 +19,8 @@ export default class App extends React.PureComponent {
     return (
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/home" component={ChatbotComponent} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/ticket/:id?" component={ChatbotComponent} />
         <UnauthRoute exact path="/login" component={Login} />
         <UnauthRoute exact path="/register" component={Registration} />
         <Route exact path="/greeting" component={ThankForRegistering} />
