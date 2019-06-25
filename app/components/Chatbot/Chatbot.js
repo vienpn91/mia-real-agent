d6c62655cac9d62a647dfd40fd71fa2f28e5006f
Tri Nguyen <trinm@zigvy.com>
2019-06-25 10:16:33 +0700

    Implement top nav bar

--- app/components/Chatbot/Chatbot.js
+++ app/components/Chatbot/Chatbot.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import {
   Layout, Icon, Input, Tooltip,
 } from 'antd';
-import PopupOverlay from 'components/Popup';
 import MessageBox from './MessageBox/MessageBox';
 import TicketItem from './TicketItem/TicketItem';
 import {
