b53b9b4ac6684b03fdbd5bfec3b098a00de08550
datnt-github <thanhdat.forwork@gmail.com>
2019-07-02 18:13:39 +0700

    Remove used code

--- app/components/TicketPage/Ticket/TicketItem/TicketItem.js
+++ app/components/TicketPage/Ticket/TicketItem/TicketItem.js
@@ -1,6 +1,5 @@
 /* eslint-disable default-case */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { object, number } from 'prop-types';
 import { Icon } from 'antd';
