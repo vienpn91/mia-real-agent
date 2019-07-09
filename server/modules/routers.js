529f6472918396808bd37b89c68a925c8f90e869
Long Hoang <longhp@zigvy.com>
2019-07-09 14:55:52 +0700

    Remove unsued components

--- server/modules/routers.js
+++ server/modules/routers.js
@@ -2,8 +2,6 @@ import express from 'express';
 /* routers */
 import UserRouter from './user/user.route';
 import AuthRouter from './auth/auth.route';
-import WitAIRouter from './witai/witai.route';
-import ConversationRouter from './conversation/conversation.route';
 import TicketRouter from './ticket/ticket.route';
 import ChatLogRouter from './chatlog/chatlog.route';
 import ApplicationRouter from './application/application.route';
@@ -13,9 +11,7 @@ const router = express.Router();
 export default () => {
   router.use('/users', UserRouter.router);
   router.use('/auth', AuthRouter.router);
-  router.use('/witai', WitAIRouter.router);
   router.use('/chats', ChatLogRouter.router);
-  router.use('/conversations', ConversationRouter.router);
   router.use('/tickets', TicketRouter.router);
   router.use('/applications', ApplicationRouter.router);
   return router;
