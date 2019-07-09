4b01885b66c5c54c90d051eb1614e7f09eb62b10
Long Hoang <longhp@zigvy.com>
2019-07-09 16:46:23 +0700

    Refactor application API

--- server/modules/application/application.route.js
+++ server/modules/application/application.route.js
@@ -4,8 +4,9 @@ import ApplicationController from './application.controller';
 class ApplicationRouter extends BaseRouter {
   constructor(controller) {
     super(controller);
-    this.router.post('/approve/:id', this.controller.approveApplication);
-    this.router.put('/:id/status/:status', this.controller.updateStatus);
+    this.router.post('/:id/approve', this.controller.approveApplication);
+    this.router.post('/:id/reject', this.controller.rejectApplication);
+    this.router.post('/:id/review', this.controller.reviewApplication);
   }
 }
 
